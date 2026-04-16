08115ef22254d01670d78394f3b3889dfc7fd2a7
wendao2004 <1601261772@qq.com>
2026-04-16 12:02:25 +0800

    新增注册业务接口

---
+++ src/main/java/com/message/news/service/UserService.java
@@ -0,0 +1,13 @@
+package com.message.news.service;
+
+import com.message.news.pojo.User;
+
+public interface UserService {
+
+    // 根据用户名查询用户
+    User findByUserName(String username);
+
+    // 注册用户
+    void register(String username, String password);
+
+}
