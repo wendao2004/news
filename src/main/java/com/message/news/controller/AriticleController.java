86bdae5b3c1e987619c50f72824d2c219e74278e
wendao2004 <1601261772@qq.com>
2026-04-18 10:20:29 +0800

    新增文章接口

---
+++ src/main/java/com/message/news/controller/AriticleController.java
@@ -0,0 +1,34 @@
+package com.message.news.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.message.news.pojo.Result;
+import com.message.news.utils.JwtUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@RequestMapping("/articles")
+public class AriticleController {
+    // 文章相关接口（CRUD、分页、搜索等）
+    @GetMapping("/list")
+    public Result<String> list(@RequestHeader(name = "Authorization", required = false) String token,
+            HttpServletResponse response) {
+        try {
+            // 验证token
+            Map<String, Object> claims = JwtUtil.parseToken(token); // 解析token，验证有效性（过期/篡改等），如果无效会抛异常
+            return Result.success("文章列表接口");
+        } catch (Exception e) {
+            // 响应状态码401 Unauthorized + 错误信息
+            response.setStatus(401);
+            return Result.fail("Token无效");
+        }
+    }
+
+}
